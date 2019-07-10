3d9f6d662917259a92532d5f93f76346b31c874e
ivanalubar <lubar.ivana94@gmail.com>
2019-07-10 21:05:52 +0200

    1. assignment - styling launch (splash) screen

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -10,95 +10,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    var counter: Int = 0
-    @IBOutlet weak var Label: UILabel!
-    @IBOutlet weak var Count: UIButton!
-    @IBOutlet weak var Reset: UIButton!
-    @IBOutlet weak var Activity: UIActivityIndicatorView!
-    @IBOutlet weak var Loading: UIButton!
-    @IBOutlet weak var StopAnimating: UIButton!
-    @IBOutlet weak var TitleLabel: UILabel!
-    @IBOutlet weak var stackView: UIStackView!
 
-    
-//    Counter reset
-    @IBAction func Reset(_ sender: UIButton) {
-        counter = 0
-        Label.text = String(self.counter)
-    }
-    
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        stackView.axis = size.width > size.height ? .horizontal : .vertical
+//        stackView.axis = size.width > size.height ? .horizontal : .vertical
 
     }
 
-//    Counter increase
-    @IBAction func Count(_ sender: UIButton) {
-        counter += 1
-        Label.text = String(self.counter)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        Activity.startAnimating()
-        Activity.isHidden = false
-        
-//        Hide while loading
-        Label.isHidden = true
-        Reset.isHidden = true
-        Count.isHidden = true
-        Loading.isHidden = true
-        StopAnimating.isHidden = true
-        StopAnimating.backgroundColor = UIColor.darkGray
-        StopAnimating.layer.cornerRadius = 5
-        StopAnimating.layer.borderWidth = 1
-        
-//        Show after 3 sec
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.Label.isHidden = false
-            self.Reset.isHidden = false
-            self.Count.isHidden = false
-            self.Loading.isHidden = false
-            self.Activity.stopAnimating()
-            self.Activity.isHidden = true
-            self.StopAnimating.isHidden = true
-        }
-        
-//        Setting background color and font
-        view.backgroundColor = UIColor.magenta
-        Label.font = UIFont(name: "Arial", size: 25)
-        Label.textColor = UIColor.black
-        TitleLabel.font = UIFont(name: "Arial", size: 15)
-        Label.text = String(counter)
-        Count.backgroundColor = .clear
-        Count.frame.size = CGSize(width: 60.0, height: 20.0)
-        Count.layer.borderColor = UIColor.lightGray.cgColor
         
     }
-    
-//    Show loading animation and hide everything else
-    @IBAction func Loading(_ sender: Any) {
-            Count.isHidden = true
-            Reset.isHidden = true
-            Label.isHidden = true
-            Activity.startAnimating()
-            Activity.isHidden = false
-            StopAnimating.isHidden = false
-            Loading.isHidden = true
-    }
-    
-//    Hide loading animation and show everything else
-    @IBAction func StopAnimating(_ sender: Any) {
-        Activity.isHidden = true
-        Activity.stopAnimating()
-        Label.isHidden = false
-        Reset.isHidden = false
-        Count.isHidden = false
-        Loading.isHidden = false
-        Activity.stopAnimating()
-        Activity.isHidden = true
-        StopAnimating.isHidden = true
-    }
+
 }
