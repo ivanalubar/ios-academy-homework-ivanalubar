5c29a84a9d97a6ff92bcf6ce505f2fad1c601ef2
ivanalubar <lubar.ivana94@gmail.com>
2019-07-11 10:19:07 +0200

    Fixes

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -8,97 +8,104 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
-    var counter: Int = 0
-    @IBOutlet weak var Label: UILabel!
-    @IBOutlet weak var Count: UIButton!
-    @IBOutlet weak var Reset: UIButton!
-    @IBOutlet weak var Activity: UIActivityIndicatorView!
-    @IBOutlet weak var Loading: UIButton!
-    @IBOutlet weak var StopAnimating: UIButton!
-    @IBOutlet weak var TitleLabel: UILabel!
-    @IBOutlet weak var stackView: UIStackView!
+    // MARK: - Outlets
+    
+    private var numberOfClicks: Int = 0
+    @IBOutlet private weak var numberOfClicksLabel: UILabel!
+    @IBOutlet private weak var touchCounterButton: UIButton!
+    @IBOutlet private weak var resetCounterButton: UIButton!
+    @IBOutlet private weak var Activity: UIActivityIndicatorView!
+    @IBOutlet private weak var loadingButton: UIButton!
+    @IBOutlet private weak var stopAnimating: UIButton!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var stackView: UIStackView!
 
-    
-//    Counter reset
-    @IBAction func Reset(_ sender: UIButton) {
-        counter = 0
-        Label.text = String(self.counter)
-    }
-    
+
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         stackView.axis = size.width > size.height ? .horizontal : .vertical
 
     }
-
-//    Counter increase
-    @IBAction func Count(_ sender: UIButton) {
-        counter += 1
-        Label.text = String(self.counter)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        startAnimtation()
+        setingView()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.configureUI()
+        }
+    }
+    
+    private func setingView(){
+        view.backgroundColor = UIColor.magenta
+        numberOfClicksLabel.font = UIFont(name: "Arial", size: 25)
+        numberOfClicksLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "Arial", size: 15)
+        numberOfClicksLabel.text = String(numberOfClicks)
+        touchCounterButton.backgroundColor = .clear
+        touchCounterButton.frame.size = CGSize(width: 60.0, height: 20.0)
+        touchCounterButton.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    private func configureUI() {
+        numberOfClicksLabel.isHidden = false
+        resetCounterButton.isHidden = false
+        touchCounterButton.isHidden = false
+        loadingButton.isHidden = false
+        Activity.stopAnimating()
+        Activity.isHidden = true
+        stopAnimating.isHidden = true
+    }
+    
+    private func startAnimtation() {
         Activity.startAnimating()
         Activity.isHidden = false
-        
-//        Hide while loading
-        Label.isHidden = true
-        Reset.isHidden = true
-        Count.isHidden = true
-        Loading.isHidden = true
-        StopAnimating.isHidden = true
-        StopAnimating.backgroundColor = UIColor.darkGray
-        StopAnimating.layer.cornerRadius = 5
-        StopAnimating.layer.borderWidth = 1
-        
-//        Show after 3 sec
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.Label.isHidden = false
-            self.Reset.isHidden = false
-            self.Count.isHidden = false
-            self.Loading.isHidden = false
-            self.Activity.stopAnimating()
-            self.Activity.isHidden = true
-            self.StopAnimating.isHidden = true
-        }
-        
-//        Setting background color and font
-        view.backgroundColor = UIColor.magenta
-        Label.font = UIFont(name: "Arial", size: 25)
-        Label.textColor = UIColor.black
-        TitleLabel.font = UIFont(name: "Arial", size: 15)
-        Label.text = String(counter)
-        Count.backgroundColor = .clear
-        Count.frame.size = CGSize(width: 60.0, height: 20.0)
-        Count.layer.borderColor = UIColor.lightGray.cgColor
-        
+        numberOfClicksLabel.isHidden = true
+        resetCounterButton.isHidden = true
+        touchCounterButton.isHidden = true
+        loadingButton.isHidden = true
+        stopAnimating.isHidden = true
+        stopAnimating.backgroundColor = UIColor.darkGray
+        stopAnimating.layer.cornerRadius = 5
+        stopAnimating.layer.borderWidth = 1
     }
     
-//    Show loading animation and hide everything else
-    @IBAction func Loading(_ sender: Any) {
-            Count.isHidden = true
-            Reset.isHidden = true
-            Label.isHidden = true
+    // MARK: - Actions
+    
+    @IBAction private func resetCounterButton(_ sender: UIButton) {
+        numberOfClicks = 0
+        numberOfClicksLabel.text = String(self.numberOfClicks)
+    }
+    
+    @IBAction private func touchCounterButtonActionHandler(_ sender: UIButton) {
+        numberOfClicks += 1
+        numberOfClicksLabel.text = String(self.numberOfClicks)
+    }
+
+    @IBAction private func loadingButtonActionHandler(_ sender: Any) {
+            touchCounterButton.isHidden = true
+            resetCounterButton.isHidden = true
+            numberOfClicksLabel.isHidden = true
             Activity.startAnimating()
             Activity.isHidden = false
-            StopAnimating.isHidden = false
-            Loading.isHidden = true
+            stopAnimating.isHidden = false
+            loadingButton.isHidden = true
     }
     
-//    Hide loading animation and show everything else
-    @IBAction func StopAnimating(_ sender: Any) {
+    @IBAction private func stopAnimatingActionHandler(_ sender: Any) {
         Activity.isHidden = true
         Activity.stopAnimating()
-        Label.isHidden = false
-        Reset.isHidden = false
-        Count.isHidden = false
-        Loading.isHidden = false
+        numberOfClicksLabel.isHidden = false
+        resetCounterButton.isHidden = false
+        touchCounterButton.isHidden = false
+        loadingButton.isHidden = false
         Activity.stopAnimating()
         Activity.isHidden = true
-        StopAnimating.isHidden = true
+        stopAnimating.isHidden = true
     }
 }
